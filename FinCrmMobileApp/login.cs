2abe01204a297510056c5e0f8201dc20ac6eaee4
Rafal Kubowicz <rafal.kubowicz@brickendon.com>
2020-04-10 13:00:03 +0200

    obsługa braku podania hasła i emaila

--- FinCrmMobileApp/login.cs
+++ FinCrmMobileApp/login.cs
@@ -15,11 +15,52 @@ namespace FinCrmMobileApp
     [Activity(Label = "Login", MainLauncher = true)]
     public class Login : Activity
     {
+        private Button _loginButton;
+        private TextView _errMsgLabel;
+        private EditText _tbEmail;
+        private EditText _tbPass;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.login);
             // Create your application here
+            FindViews();
+            LinkEventHandlers();
+        }
+
+        private void FindViews()
+        {
+            _loginButton = FindViewById<Button>(Resource.Id.btn_login);
+            _errMsgLabel = FindViewById<TextView>(Resource.Id.error_msg);
+            _tbEmail = FindViewById<EditText>(Resource.Id.input_email);
+            _tbPass = FindViewById<EditText>(Resource.Id.input_password);
+
+        }
+
+        private void LinkEventHandlers()
+        {
+            _loginButton.Click += _loginButton_Click;
+        }
+
+        private void _loginButton_Click(object sender, EventArgs e)
+        {
+            _errMsgLabel.Visibility = ViewStates.Invisible;
+            if (_tbEmail.Text.Length < 3 )
+            {
+                _errMsgLabel.Text = "Najpierw wprowadź email";
+                _errMsgLabel.Visibility = ViewStates.Visible;
+                return;
+            }
+
+            if (_tbPass.Text.Length < 3)
+            {
+                _errMsgLabel.Text = "Podaj hasło";
+                _errMsgLabel.Visibility = ViewStates.Visible;
+                return;
+            }
+
+
         }
     }
 }
